456f79efdab4b322c9a2db7f7e7336d96b22ae42
Bulk Developer <jaydenpatrick1110@gmail.com>
2016-07-31 16:38:12 -0400

    Update Task.php

--- src/hud/Task.php
+++ src/hud/Task.php
@@ -11,37 +11,25 @@ use pocketmine\utils\Config;
 
 Class Task extends PluginTask{
 
-    public $economy;
-
+    const COUNT = 0;
 
     public function __construct($plugin)
     {
         $this->plugin = $plugin;
         parent::__construct($plugin);
-       if($this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI" )!= null){
-        $this->money = \onebone\economyapi\EconomyAPI::getInstance();
-       $this->getOwner()->getLogger()->info("[X-Hud]EconomyS has been detected variable {MONEY} activated");
-     }else{
-       $this->getOwner()->getLogger()->info("[X-Hud]EconomyS has NOT been detected variable {MONEY} deactivated");
-       }
+        $this->count = Task::COUNT;
             
     }
 public function onRun($currentTick)
 {
     foreach ($this->getOwner()->getServer()->getOnlinePlayers() as $p){
-        $message = $this->plugin->getConfig()->get("Message");
-           $faggot = str_replace("{X}",round($p->getX()),$message);
-           $faggo = str_replace("{Y}",round($p->getY()),$faggot);
-           $fagg = str_replace("{Z}",round($p->getZ()),$faggo);
-           $fag = str_replace("{NAME}",$p->getName(),$fagg);
-           $fa = str_replace("{WORLD}",$p->getLevel()->getName(),$fag);
-           $f = str_replace("{NEXTLINE}","\n",$fa);
-            if($this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI") != null){
-           $fu = str_replace("{MONEY}",$this->money->myMoney($p),$f);
-        $p->sendPopup($fu);
-          }else{
-          $p->sendPopup($f);
-}
+        $message = $this->plugin->getMessage($this->count, $p);
+        $p->sendTip($message);
+        $this->count++;
+       }
+       
+       if($this->count >= $this->plugin->count){
+           $this->count-=$this->plugin->count;
        }
         }
 
